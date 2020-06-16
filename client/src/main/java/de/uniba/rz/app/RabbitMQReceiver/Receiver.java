fba3c438df125fbd3741d580ead24be2b8c91ee0
Shahtaj Qasim <61311949+shahtaj-qasim@users.noreply.github.com>
2020-06-16 18:07:15 +0200

    Receiver revised

--- client/src/main/java/de/uniba/rz/app/RabbitMQReceiver/Receiver.java
+++ client/src/main/java/de/uniba/rz/app/RabbitMQReceiver/Receiver.java
@@ -1,76 +1,70 @@
-package de.uniba.rz.app.RabbitMQSender;
-import com.rabbitmq.client.ConnectionFactory;
-
-import de.uniba.rz.app.RabbitMQReceiver.Receiver;
-
-import com.rabbitmq.client.Connection;
+package de.uniba.rz.app.RabbitMQReceiver;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.UUID;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeoutException;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
-public class Sender {
+public class Receiver {
 	
 	private final static String QUEUE_NAME = "hello-world";
-	private static Connection connection;
-	
-	public static String main(String[] args) throws IOException, TimeoutException, InterruptedException {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
 		// TODO Auto-generated method stub
-		System.out.println("In sender MQConnection method");
+		System.out.println("In Receiver main method");
 		ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        connection = factory.newConnection(); 
+        Connection connection = factory.newConnection(); 
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null); //if you start consumer before sender, then u wont have any queues so you can just leave this here 
+        channel.queuePurge(QUEUE_NAME);
         
-        try (Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String callbackQueueName = channel.queueDeclare().getQueue();
-            final String corrId = UUID.randomUUID().toString();
-            BasicProperties props = new BasicProperties
-                            .Builder()
-                            .correlationId(corrId)
-                            .replyTo(callbackQueueName)
-                            .build();
-            String message= "Hello world, wanna work?" + LocalDateTime.now();
-            channel.basicPublish("", QUEUE_NAME, props, message.getBytes("UTF-8"));
-            System.out.println(" !!! Sent message:  '" + message + "'");
-            
-           //Receiver rcv= new Receiver();
-            //rcv.MQconnectionReceiveMessage();
-            // code to read a response message from the queue
-            final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
-
-            String ctag = channel.basicConsume(callbackQueueName, true, (consumerTag, delivery) -> {
-                if (delivery.getProperties().getCorrelationId().equals(corrId)) {
-                    response.offer(new String(delivery.getBody(), "UTF-8"));
+        //channel.basicQos(1);
+        
+        System.out.println(" !! Waiting for message");
+        
+        Object monitor = new Object();
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            BasicProperties replyProps = new BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+            String response = "This is a response from server";
+            try {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" !! Received message: '" + message + "'");
+            }
+            catch (RuntimeException e) {
+                System.out.println(" [.] " + e.toString());
+            }finally {
+   //     DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+   //         String message = new String(delivery.getBody(), "UTF-8");
+    //        System.out.println(" !! Received message: '" + message + "'");
+            channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            // RabbitMq consumer worker thread notifies the RPC server owner thread
+            synchronized (monitor) {
+                monitor.notify();
+            }
+            }
+        };
+        System.out.println(" !! Received message: ");
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, (consumerTag -> { }));
+        while (true) {
+            synchronized (monitor) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }, consumerTag -> {
-            });
-
-            String result = response.take();
-            channel.basicCancel(ctag);
-            System.out.println("get response sender method: "+result);
-            return result;
-           
-        } catch (IOException e) {
-			// TODO Auto-generated catch block
-        	System.out.println(e.getMessage());
-			e.printStackTrace();
-			return e.getMessage();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			return e.getMessage();
-		}
+            }
+        }
+            
+        
 	}
-	public void close() throws IOException {
-        connection.close();
-    }
 
 }
